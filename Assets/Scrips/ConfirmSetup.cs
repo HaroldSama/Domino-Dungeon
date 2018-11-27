34ea6ff8c915e8b94a5714a2a684dcaf29582f14
HaroldSama <rz886@nyu.edu>
2018-11-26 19:57:52 -0500

    UI added

--- Assets/Scrips/ConfirmSetup.cs
+++ Assets/Scrips/ConfirmSetup.cs
@@ -15,24 +15,31 @@ public class ConfirmSetup : MonoBehaviour {
 		
 	}
 	
-	public void OnMouseDown()
+	public void OnClick()
 	{
 		print("Confirm Setup");
-		GameObject.Find("Starting Block").GetComponent<Push>().Setup = true;
+		GameObject.Find("Starting Block").GetComponent<Push>().Setup = true;//Make the starting block able to be push
 		
 		GameObject[] Sets = GameObject.FindGameObjectsWithTag("Set");
 		Debug.Log(Sets.Length);
 		for (int i = 0; i < Sets.Length; i++)
 		{
 			Sets[i].GetComponent<MeshCollider>().enabled = false;
-		}
+		}//Disable the collider of domino sets
 		
 		GameObject[] Dominos = GameObject.FindGameObjectsWithTag("Domino");
 		Debug.Log(Dominos.Length);
 		for (int i = 0; i < Dominos.Length; i++)
 		{
 			Dominos[i].GetComponent<Rigidbody>().isKinematic = false;
-		}
+		}//Enable physics of domino
+		
+		GameObject[] UIs = GameObject.FindGameObjectsWithTag("UI");
+		Debug.Log(UIs.Length);
+		for (int i = 0; i < UIs.Length; i++)
+		{
+			Destroy(UIs[i]);
+		}//Remove UI
 	}
 	
 }
---
+++ Assets/Scrips/CreateSets.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateSets : MonoBehaviour {
+	
+	List<GameObject> Sets = new List<GameObject>();
+	public int Amount = 4;
+
+	// Use this for initialization
+	void Start () {
+		
+	GetComponentInChildren<Text>().text = "" + Amount;	
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+
+	
+		public void OnMouseUp()
+	{
+		if (Amount > 0)
+		{
+			print("Generate " + name);
+    		GameObject set = Instantiate(Resources.Load<GameObject>("Prefabs/" + name)); //create a new Set
+    		Amount = Amount - 1;
+    		GetComponentInChildren<Text>().text = "" + Amount;		
+			Sets.Add(set);
+		}
+	}
+}
