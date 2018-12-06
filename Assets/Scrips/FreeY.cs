c1e6b24786c5bc2feb0995e847c47d1b892d3c13
177sma <howerds4my@gmail.com>
2018-12-05 22:19:17 -0500

    Level 2 Finished

---
+++ Assets/Scrips/FreeY.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreeY : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (GetComponent<Rigidbody>().velocity.magnitude > 0.1)
+		{
+			StartCoroutine(waiterLose());
+		}
+
+	}
+	
+	IEnumerator waiterLose()
+	{
+		yield return new WaitForSeconds(3);
+		GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+	}
+}
---
+++ Assets/Scrips/Smog.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smog : MonoBehaviour
+{
+
+	public ParticleSystem ParticleLauncher;
+	public ContactPoint contact;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		//ContactPoint contact = collision.contact.point;
+		Instantiate(Resources.Load<GameObject>("Particle/SmogEmitSmall"), collision.contacts[0].point, new Quaternion(0,0,0,0));
+		GetComponent<AudioSource>().Play();
+	}
+	
+}
+
